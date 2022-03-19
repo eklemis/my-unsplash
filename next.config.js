23a9c54b214c0d0680fc02cb1cd1f182d0694ed1
Eklemis Santo Ndun <Eklemis.ndun@gmail.com>
2022-03-20 06:05:18 +0800

    triger redeploy

--- next.config.js
+++ next.config.js
@@ -6,6 +6,6 @@ const nextConfig = {
 module.exports = nextConfig;
 module.exports = {
 	images: {
-		domains: ["cdn.pixabay.com", "i.ibb.co", ""],
+		domains: ["cdn.pixabay.com", "i.ibb.co"],
 	},
 };
